866ec6ea234f7f006892c29f2f924d1d7a4d18ef
ljoe <luke.lukejoe@gmail.com>
2026-01-07 00:01:54 -0700

    setup main scene + simple movement script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tank game
+
+### TODO:
+#### Part 1: Groundwork
+	[] setup movement
+	[] setup turret controls
+	[] shoot
+	[] hp
+	
+#### Part 2: Peer to peer networking
+
+
+#### Part 3: Polish
--- scripts/tank/tank.gd
+++ scripts/tank/tank.gd
@@ -1,28 +1,41 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+@export var move_speed := 750.0
+@export var turn_speed := 2.5
+@export var acceleration := 50000.0
+@export var deceleration := 150.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	handle_rotation(delta)
+	handle_movement(delta)
 	move_and_slide()
+
+func handle_movement(delta):
+	var input := 0.0
+	if Input.is_action_pressed("move_forward"):
+		input += 1.0
+	if Input.is_action_pressed("move_backward"):
+		input -= 1.0
+		
+	var forward := -transform.basis.z
+	var target_speed := input * move_speed
+	var current_speed := velocity.dot(forward)
+	
+	var accel := acceleration if abs(input) > 0 else deceleration
+	var new_speed := move_toward(current_speed, target_speed, accel * delta)
+	
+	velocity = forward * new_speed
+	velocity.y = 0.0
+		
+func handle_rotation(delta):
+	var reversing := Input.is_action_pressed("move_backward")
+	
+	var turn := 0.0
+	if Input.is_action_pressed("turn_left"):
+		turn += 1.0
+	if Input.is_action_pressed("turn_right"):
+		turn -= 1.0
+		
+	turn *= -1.0 if reversing else 1.0
+		
+	rotation.y += turn * turn_speed * delta
