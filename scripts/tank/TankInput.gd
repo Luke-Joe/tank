eb045524f1a6aae39ca904433c52c34d5f1df1ba
luke <luke.lukejoe@gmail.com>
2026-02-19 20:39:13 -0700

    formatting

--- scripts/tank/TankInput.gd
+++ scripts/tank/TankInput.gd
@@ -5,24 +5,28 @@ var move := 0.0
 var turn := 0.0
 
 # turret
-var aim_world_pos : Vector3
+var aim_world_pos: Vector3
 var fire := false
 
 var state = TankInputState.new()
 
-func _physics_process(delta: float) -> void:
+
+func _physics_process(_delta: float) -> void:
 	if not get_parent().is_multiplayer_authority() or not is_inside_tree():
-		return	
-	
-	state.move = Input.get_action_strength("move_forward") - Input.get_action_strength("move_backward")
+		return
+
+	state.move = (
+		Input.get_action_strength("move_forward") - Input.get_action_strength("move_backward")
+	)
 	state.turn = Input.get_action_strength("turn_left") - Input.get_action_strength("turn_right")
 	state.aim = get_mouse_world_position()
 	state.fire = Input.is_action_pressed("fire")
 
+
 func get_mouse_world_position() -> Vector3:
 	var viewport := get_viewport()
 	var camera := viewport.get_camera_3d()
-	
+
 	var mouse_pos := viewport.get_mouse_position()
 	var ray_origin := camera.project_ray_origin(mouse_pos)
 	var ray_dir := camera.project_ray_normal(mouse_pos)
