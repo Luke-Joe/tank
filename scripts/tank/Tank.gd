022014e6704cc29c537aa3170888962214a9e55a
luke <luke.lukejoe@gmail.com>
2026-02-20 22:55:25 -0700

    change shell spawn to use rpc

--- scripts/tank/Tank.gd
+++ scripts/tank/Tank.gd
@@ -17,19 +17,20 @@ extends CharacterBody3D
 @export var max_active_shells := 100
 
 var active_shells := 0
-var input_state : TankInputState
+var input_state: TankInputState
 var player_id := 0
 var can_fire := true
 var is_dead := false
 
+
 func _ready() -> void:
 	health.died.connect(_on_died)
-	
+
 
 func _physics_process(delta: float) -> void:
 	if not is_inside_tree():
 		return
-	
+
 	_handle_input()
 	if input_state == null:
 		return
@@ -37,70 +38,75 @@ func _physics_process(delta: float) -> void:
 	_handle_movement(delta)
 	move_and_slide()
 
+
 func _handle_input() -> void:
 	if input_node:
 		input_state = input_node.state
-		
+
 	if input_state == null:
 		return
-	
+
 	turret.apply_input(input_state)
-	if (input_state.fire):
+	if input_state.fire:
 		_request_fire()
-	
+
+
 func _handle_movement(delta):
 	var forward := -transform.basis.z
 	var target_speed := input_state.move * move_speed
 	var current_speed := velocity.dot(forward)
-	
+
 	var accel := acceleration if abs(input_state.move) > 0 else deceleration
 	var new_speed := move_toward(current_speed, target_speed, accel * delta)
-	
+
 	velocity = forward * new_speed
 	velocity.y = 0.0
-		
+
+
 func _handle_rotation(delta) -> void:
 	var is_reversing := input_state.move < 0
-	
+
 	var turn = input_state.turn
 	turn *= -1.0 if is_reversing else 1.0
-	
+
 	rotation.y += turn * turn_speed * delta
+
 
 func _request_fire() -> void:
 	if not can_fire or active_shells >= max_active_shells:
 		return
-		
+
 	can_fire = false
-	_fire_shell()
+	_fire_shell.rpc(muzzle.global_position, -muzzle.global_basis.z)
 	await get_tree().create_timer(fire_cooldown).timeout
 	can_fire = true
+
 
 func receive_damage(hit: HitInfo) -> void:
 	health._apply_damage(hit)
 
-func _fire_shell() -> void:
-	if (!shell_scene):
+
+@rpc("any_peer", "reliable", "call_local")
+func _fire_shell(spawn_position: Vector3, direction: Vector3) -> void:
+	if !shell_scene:
 		push_error("shell_scene not set on tank")
 		return
-		
+
 	var shell = shell_scene.instantiate()
-	shell.global_position = muzzle.global_position
+	shell.global_position = spawn_position
 	get_tree().current_scene.add_child(shell)
-	shell.fire(-muzzle.global_basis.z, player_id)
+	shell.fire(direction, player_id)
 	shell.shell_despawned.connect(_on_shell_despawned)
-	
 	active_shells += 1
-	print("firing shell")
-	
+
+
 func _on_shell_despawned(shell: Node) -> void:
 	active_shells -= 1
-	print('active shells:', active_shells)
-	
+	print("active shells:", active_shells)
+
+
 func _on_died(source_id: int) -> void:
-	if is_dead: 
+	if is_dead:
 		return
-	
+
 	is_dead = true
-	
-	
