8d352856f21c25e3b5ba6eb91cfa4bec8d26e654
luke <luke.lukejoe@gmail.com>
2026-05-01 17:53:39 -0600

    initial work for communicating with relay server

--- scripts/networking/relay_multiplayer_peer.gd
+++ scripts/networking/relay_multiplayer_peer.gd
@@ -8,6 +8,7 @@ var _target_peer := 0
 var _status := MultiplayerPeer.CONNECTION_DISCONNECTED
 var _incoming := []
 var _join_code := ""
+var _is_host := false
 
 
 func host(relay_url: String) -> void:
@@ -36,6 +37,40 @@ func _handle_message(msg: Dictionary) -> void:
 	var msg_type = RelayMessageType.string_to_server(msg.get("type", ""))
 
 	match msg_type:
-		RelayMessage.Server.ID_ASSIGNED:
+		RelayMessageType.Server.ID_ASSIGNED:
 			_unique_id = msg.get("id")
 			if _is_host:
+				_send({"type": RelayMessageType.client_to_string(RelayMessageType.Client.CREATE)})
+			else:
+				_send(
+					{
+						"type": RelayMessageType.client_to_string(RelayMessageType.Client.JOIN),
+						"joinCode": _join_code
+					}
+				)
+		RelayMessageType.Server.ROOM_JOINED:
+			_host_id = msg.get("hostId")
+			_status = MultiplayerPeer.CONNECTION_CONNECTED
+
+		RelayMessageType.Server.PEER_CONNECTED:
+			var peer_id: int = msg.get("peerId")
+			emit_signal("peer_connected", peer_id)
+
+		RelayMessageType.Server.PEER_DISCONNECTED:
+			var peer_id: int = msg.get("peerId")
+			emit_signal("peer_disconnected", peer_id)
+
+		RelayMessageType.Server.RELAY:
+			var from: int = msg.get("from")
+			var data: PackedByteArray = Marshalls.base64_to_raw(msg.get("data", ""))
+			_incoming.append({"peer": from, "data": data})
+
+		RelayMessageType.Server.ERROR:
+			push_error("Relay error: " + str(msg.get("message", "")))
+
+		_:
+			push_error("Unhandled message type: " + str(msg.get("type", "")))
+
+
+func _send(data: Dictionary) -> void:
+	_socket.send_text(JSON.stringify(data))
